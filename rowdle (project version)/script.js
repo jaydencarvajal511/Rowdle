3f4baf407af09cfdf94b83409c9dea487b68f490
Jayden Carvajal <147960616+jaydencarvajal511@users.noreply.github.com>
2023-10-15 23:47:01 -0400

    Add files via upload

---
+++ rowdle (project version)/script.js
@@ -0,0 +1,437 @@
+let rowers = [
+  {
+    name: "Moses Ward",
+    team: "Freshmen",
+    boat: "Gonzalez",
+    side: "Port",
+    seat: 2,
+    grade: 9,
+    height: "5'10",
+    heightInInches: 70,
+  },
+  {
+    name: "Terrence Parsons",
+    team: "Varsity",
+    boat: "Full Moon",
+    side: "Scull",
+    seat: 1,
+    grade: 10,
+    height: "5'9",
+    heightInInches: 69,
+  },
+  {
+    name: "Theodore French",
+    team: "Freshmen",
+    boat: "Capillo",
+    side: "N/A",
+    seat: "Cox",
+    grade: 9,
+    height: "5'3",
+    heightInInches: 63,
+  },
+  {
+    name: "Todd Massey",
+    team: "Freshmen",
+    boat: "Capillo",
+    side: "Starboard",
+    seat: 7,
+    grade: 9,
+    height: "6'1",
+    heightInInches: 73,
+  },
+  {
+    name: "Jordan White",
+    team: "Varsity",
+    boat: "Loaner",
+    side: "Starboard",
+    seat: 3,
+    grade: 10,
+    height: "6'2",
+    heightInInches: 74,
+  },
+  {
+    name: "Jayden Carvajal",
+    team: "Varsity",
+    boat: "Hudson",
+    side: "N/A",
+    seat: "Cox",
+    grade: 10,
+    height: "5'6",
+    heightInInches: 66,
+  },
+  {
+    name: "Cornelius Morris",
+    team: "Varsity",
+    boat: "Danger Zone",
+    side: "Starboard",
+    seat: 1,
+    grade: 10,
+    height: "5'8",
+    heightInInches: 68,
+  },
+  {
+    name: "Adam Park",
+    team: "Freshmen",
+    boat: "Gonzalez",
+    side: "Port",
+    seat: 4,
+    grade: 9,
+    height: "5'10",
+    heightInInches: 70,
+  },
+  {
+    name: "Jamie Munoz",
+    team: "Varsity",
+    boat: "Hudson",
+    side: "Starboard",
+    seat: 1,
+    grade: 11,
+    height: "6'1",
+    heightInInches: 73,
+  },
+  {
+    name: "Brian Anderson",
+    team: "Varsity",
+    boat: "Loaner",
+    side: "Port",
+    seat: 2,
+    grade: 11,
+    height: "5'10",
+    heightInInches: 70,
+  },
+  {
+    name: "Terry Carr",
+    team: "Freshmen",
+    boat: "Capillo",
+    side: "Starboard",
+    seat: 1,
+    grade: 9,
+    height: "5'10",
+    heightInInches: 70,
+  },
+  {
+    name: "Garry Allison",
+    team: "Freshmen",
+    boat: "Gonzalez",
+    side: "Port",
+    seat: 6,
+    grade: 9,
+    height: "5'11",
+    heightInInches: 71,
+  },
+  {
+    name: "Santiago Pena",
+    team: "Freshmen",
+    boat: "Gonzalez",
+    side: "Starboard",
+    seat: 1,
+    grade: 9,
+    height: "5'9",
+    heightInInches: 69,
+  },
+  {
+    name: "Alberto Ortega",
+    team: "Freshmen",
+    boat: "Capillo",
+    side: "Starboard",
+    seat: 5,
+    grade: 9,
+    height: "5'11",
+    heightInInches: 71,
+  },
+  {
+    name: "Julius Hill",
+    team: "Freshmen",
+    boat: "Capillo",
+    side: "Port",
+    seat: 6,
+    grade: 9,
+    height: "5'8",
+    heightInInches: 68,
+  },
+  {
+    name: "Kurt Simmons",
+    team: "Varsity",
+    boat: "Danger Zone",
+    side: "N/A",
+    seat: "Cox",
+    grade: 9,
+    height: "5'7",
+    heightInInches: 67,
+  },
+  {
+    name: "Joey Gray",
+    team: "Varsity",
+    boat: "Loaner",
+    side: "Starboard",
+    seat: 1,
+    grade: 11,
+    height: "4'7",
+    heightInInches: 55,
+  },
+  {
+    name: "Paul Briggs",
+    team: "Freshmen",
+    boat: "Gonzalez",
+    side: "Starboard",
+    seat: 3,
+    grade: 9,
+    height: "5'6",
+    heightInInches: 66,
+  },
+  {
+    name: "Matthew Allen",
+    team: "Freshmen",
+    boat: "Gonzalez",
+    side: "N/A",
+    seat: "Cox",
+    grade: 9,
+    height: "5'4",
+    heightInInches: 64,
+  },
+  {
+    name: "Walter Sanchez",
+    team: "Freshmen",
+    boat: "Gonzalez",
+    side: "Port",
+    seat: 8,
+    grade: 9,
+    height: "5'9",
+    heightInInches: 69,
+  },
+  {
+    name: "Jared Perry",
+    team: "Varsity",
+    boat: "Danger Zone",
+    side: "Port",
+    seat: 4,
+    grade: 10,
+    height: "6'0",
+    heightInInches: 72,
+  },
+  {
+    name: "Merle Jenkins",
+    team: "Freshmen",
+    boat: "Capillo",
+    side: "Port",
+    seat: 8,
+    grade: 9,
+    height: "5'11",
+    heightInInches: 71,
+  },
+  {
+    name: "Clarence Christensen",
+    team: "Freshmen",
+    boat: "Gonzalez",
+    side: "Starboard",
+    seat: 7,
+    grade: 9,
+    height: "5'9",
+    heightInInches: 69,
+  },
+  {
+    name: "Enrique Hale",
+    team: "Freshmen",
+    boat: "Capillo",
+    side: "Starboard",
+    seat: 3,
+    grade: 9,
+    height: "6'1",
+    heightInInches: 73,
+  },
+  {
+    name: "Troy Castro",
+    team: "Varsity",
+    boat: "Hudson",
+    side: "Starboard",
+    seat: 3,
+    grade: 10,
+    height: "6'5",
+    heightInInches: 77,
+  },
+  {
+    name: "Ken Wells",
+    team: "Freshmen",
+    boat: "Capillo",
+    side: "Port",
+    seat: 2,
+    grade: 9,
+    height: "5'8",
+    heightInInches: 68,
+  },
+  {
+    name: "Larry Martinez",
+    team: "Varsity",
+    boat: "Full Moon",
+    side: "Scull",
+    seat: 2,
+    grade: 10,
+    height: "5'5",
+    heightInInches: 65,
+  },
+  {
+    name: "Alton Gibson",
+    team: "Varsity",
+    boat: "Hudson",
+    side: "Port",
+    seat: 4,
+    grade: 10,
+    height: "5'11",
+    heightInInches: 71,
+  },
+  {
+    name: "Edmund Perkins",
+    team: "Freshmen",
+    boat: "Capillo",
+    side: "Port",
+    seat: 4,
+    grade: 9,
+    height: "6'0",
+    heightInInches: 72,
+  },
+  {
+    name: "Boyd Flores",
+    team: "Varsity",
+    boat: "Danger Zone",
+    side: "Starboard",
+    seat: 3,
+    grade: 10,
+    height: "6'4",
+    heightInInches: 76,
+  },
+  {
+    name: "Tyrone Riley",
+    team: "Varsity",
+    boat: "Danger Zone",
+    side: "Port",
+    seat: 2,
+    grade: 10,
+    height: "5'10",
+    heightInInches: 70,
+  },
+  {
+    name: "Arnold Richardson",
+    team: "Freshmen",
+    boat: "Gonzalez",
+    side: "Starboard",
+    seat: 5,
+    grade: 9,
+    height: "5'5",
+    heightInInches: 65,
+  },
+  {
+    name: "Otis Evans",
+    team: "Varsity",
+    boat: "Hudson",
+    side: "Port",
+    seat: 2,
+    grade: 11,
+    height: "6'2",
+    heightInInches: 74,
+  },
+  {
+    name: "Blake Watson",
+    team: "Freshmen",
+    boat: "Gonzalez",
+    side: "Port",
+    seat: 4,
+    grade: 11,
+    height: "5'11",
+    heightInInches: 71,
+  },
+];
+
+let index = Math.floor(Math.random(0) * 22);
+let attempt = 1;
+let won = false;
+
+function submit() {
+  if (attempt < 5 && !won) {
+    answer = rowers[index];
+    guess = rowers[Number(document.getElementById("guess").value)];
+    document.querySelector(`.guess-${attempt}`).style.display = "grid";
+    document.querySelector(`.guess-${attempt}>.box-1`).innerText = guess.name;
+    document.querySelector(`.guess-${attempt}>.box-2`).innerText = guess.team;
+    document.querySelector(`.guess-${attempt}>.box-3`).innerText = guess.boat;
+    document.querySelector(`.guess-${attempt}>.box-4`).innerText = guess.side;
+    document.querySelector(`.guess-${attempt}>.box-5`).innerText = guess.seat;
+    document.querySelector(`.guess-${attempt}>.box-6`).innerText = guess.grade;
+    document.querySelector(`.guess-${attempt}>.box-7`).innerText = guess.height;
+
+    if (guess.name == answer.name) {
+      document.querySelector(`.guess-${attempt}>.box-1`).style.background = "green";
+      won = true;
+    }
+    if (guess.team == answer.team) {
+      document.querySelector(`.guess-${attempt}>.box-2`).style.background = "green";
+    }
+    if (guess.boat == answer.boat) {
+      document.querySelector(`.guess-${attempt}>.box-3`).style.background = "green";
+    }
+    if (guess.side == answer.side) {
+      document.querySelector(`.guess-${attempt}>.box-4`).style.background = "green";
+    }
+    if (guess.seat == answer.seat) {
+      document.querySelector(`.guess-${attempt}>.box-5`).style.background = "green";
+    } else if (guess.seat == answer.seat + 1) {
+      document.querySelector(`.guess-${attempt}>.box-5`).style.background = "yellow";
+      document.querySelector(`.guess-${attempt}>.box-5`).innerText = guess.seat + " ↓";
+    } else if (guess.seat == answer.seat - 1) {
+      document.querySelector(`.guess-${attempt}>.box-5`).style.background = "yellow";
+      document.querySelector(`.guess-${attempt}>.box-5`).innerText = guess.seat + " ↑";
+    } else if (guess.seat > answer.seat) {
+      document.querySelector(`.guess-${attempt}>.box-5`).innerText = guess.seat + " ↓";
+    } else if (guess.seat < answer.seat) {
+      document.querySelector(`.guess-${attempt}>.box-5`).innerText = guess.seat + " ↑";
+    }
+    if (answer.seat == "Cox") {
+      document.querySelector(`.guess-${attempt}>.box-5`).innerText = guess.seat + " ↑";
+    }
+    if (guess.grade == answer.grade) {
+      document.querySelector(`.guess-${attempt}>.box-6`).style.background = "green";
+    } else if (guess.grade == answer.grade + 1) {
+      document.querySelector(`.guess-${attempt}>.box-6`).style.background = "yellow";
+      document.querySelector(`.guess-${attempt}>.box-6`).innerText = guess.grade + " ↓";
+    } else if (guess.grade == answer.grade - 1) {
+      document.querySelector(`.guess-${attempt}>.box-6`).style.background = "yellow";
+      document.querySelector(`.guess-${attempt}>.box-6`).innerText = guess.grade + " ↑";
+    } else if (guess.grade > answer.grade) {
+      document.querySelector(`.guess-${attempt}>.box-6`).innerText = guess.grade + " ↓";
+    } else if (guess.grade < answer.grade) {
+      document.querySelector(`.guess-${attempt}>.box-6`).innerText = guess.grade + " ↑";
+    }
+    if (guess.height == answer.height) {
+      document.querySelector(`.guess-${attempt}>.box-7`).style.background = "green";
+    } else if (guess.heightInInches == answer.heightInInches + 1) {
+      document.querySelector(`.guess-${attempt}>.box-7`).style.background = "yellow";
+      document.querySelector(`.guess-${attempt}>.box-7`).innerText = guess.height + " ↓";
+    } else if (guess.heightInInches == answer.heightInInches - 1) {
+      document.querySelector(`.guess-${attempt}>.box-7`).style.background = "yellow";
+      document.querySelector(`.guess-${attempt}>.box-7`).innerText = guess.height + " ↑";
+    } else if (guess.heightInInches > answer.heightInInches) {
+      document.querySelector(`.guess-${attempt}>.box-7`).innerText = guess.height + " ↓";
+    } else if (guess.heightInInches < answer.heightInInches) {
+      document.querySelector(`.guess-${attempt}>.box-7`).innerText = guess.height + " ↑";
+    }
+
+    attempt++;
+  }
+  document.getElementById("guess").value = "select";
+  if (won) {
+    document.querySelector(`.win-screen`).style.display = "inherit";
+    document.querySelector(`.person`).innerText = answer.name;
+  } else if (attempt == 5) {
+    document.querySelector(`.lose-screen`).style.display = "inherit";
+    document.querySelector(`.correct-person`).innerText = answer.name;
+  }
+}
+
+function exit() {
+  document.querySelector(".lose-screen").style.display = "none";
+  document.querySelector(".win-screen").style.display = "none";
+  document.querySelector(".manual-screen").style.display = "none";
+}
+
+function manual() {
+  document.querySelector(".manual-screen").style.display = "inherit";
+}
